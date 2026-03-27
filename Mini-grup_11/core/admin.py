30255f4b7b36a9afedf4dad51d5666553beae6b1
Rozivoy <dehqonaliyevrozivoy33@gmail.com>
2026-03-27 10:25:19 +0500

    first

--- Mini-grup_11/core/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models.auth_models import User
-from .models.models import Product, Compare, ProductComment, Promotion, CartItem, Category, Contact, Cart
-# Register your models here.
-admin.site.register(User)
--- Mini-grup_11/core/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# Импортируем все модели, чтобы Django "увидел" их при создании миграций
-from .models import *
-from .auth_models import *
