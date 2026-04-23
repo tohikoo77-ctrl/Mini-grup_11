3e5a7a70da6915f5fd7bb797ad4b5544b1be940c
Abdulmajid Yunus <goldendevuz@gmail.com>
2026-04-23 18:35:21 +0500

    2026.04.23/18:35

--- core/settings.py
+++ core/settings.py
@@ -181,6 +181,7 @@ REST_FRAMEWORK = {
 
     # 🎨 Renderers (JSON:API first)
     "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
         "rest_framework_json_api.renderers.JSONRenderer",
         "rest_framework_json_api.renderers.BrowsableAPIRenderer",  # disable in prod if not needed
     ),
